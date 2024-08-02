1d48a9bff6375f2cd1b96ca42d772f5f63f591ea
nikhil bisht <nikhilbisht740@gmail.com>
2024-08-02 22:10:00 +0530

    .

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import BannerEditor from "@/components/BannerEditor";
 // import { log } from "console";
 
