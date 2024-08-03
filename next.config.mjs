d076d8eabb8dd7f80dfd43c2a3326b7dc71363d7
nikhil bisht <nikhilbisht740@gmail.com>
2024-08-03 14:09:10 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["source.unsplash.com"], // Add any other domains you're loading images from
-//   },
-// };
+// const nextConfig = {};
 
 // export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["source.unsplash.com"], // Add any other domains you're loading images from
+  },
+};
+
+export default nextConfig;
--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -48,13 +48,13 @@ import BannerEditor from "@/components/BannerEditor";
 import { Suspense } from "react";
 
 const banners = [
-  { id: 1, imageUrl: "/images/banner1.jpg", title: "Summer BBQ" },
-  { id: 2, imageUrl: "/images/banner2.jpg", title: "Cozy Winter" },
-  { id: 3, imageUrl: "/images/banner3.jpg", title: "Spring Picnic" },
-  { id: 4, imageUrl: "/images/banner4.jpg", title: "Autumn Harvest" },
-  { id: 5, imageUrl: "/images/banner6.jpg", title: "Festive Celebration" },
-  { id: 6, imageUrl: "/images/banner7.jpg", title: "Festive Celebration" },
-  { id: 7, imageUrl: "/images/banner8.jpg", title: "Festive Celebration" },
+  { id: 1, imageUrl: "/imag/banner1.jpg", title: "Summer BBQ" },
+  { id: 2, imageUrl: "/imag/banner2.jpg", title: "Cozy Winter" },
+  { id: 3, imageUrl: "/imag/banner3.jpg", title: "Spring Picnic" },
+  { id: 4, imageUrl: "/imag/banner4.jpg", title: "Autumn Harvest" },
+  { id: 5, imageUrl: "/imag/banner6.jpg", title: "Festive Celebration" },
+  { id: 6, imageUrl: "/imag/banner7.jpg", title: "Festive Celebration" },
+  { id: 7, imageUrl: "/imag/banner8.jpg", title: "Festive Celebration" },
 ];
 
 function BannerContent() {
